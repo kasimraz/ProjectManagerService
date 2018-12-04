16962dd1b167792ea3637d6a48c6ce3fd9778460
Kasim <mohammedkasim23@gmail.com>
2018-12-04 12:55:12 +0530

    Checking in Web Api code first time

---
+++ ProgramManagerUnitTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectManagerWebApi.Controllers;
+using ProjectManagerWebApi;
+using System.Collections.Generic;
+
+namespace ProgramManagerUnitTest
+{
+    [TestClass]
+    public class UserControllerTest
+
+    {
+
+
+        [TestMethod]
+        public void GetUsers()
+        {
+            UsersController user = new UsersController();
+             List<User> users= user.GetUsers();
+            Assert.IsNotNull(users);
+        }
+    }
+}
---
+++ ProjectManagerWebApi/Controllers/UsersController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ProjectManagerWebApi;
+using System.Web.Http.Cors;
+
+namespace ProjectManagerWebApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class UsersController : ApiController
+    {
+        private ProjectManagerEntities db = new ProjectManagerEntities();
+
+        // GET: api/Users
+        public List<User> GetUsers()
+        {
+            var users = db.Users.ToList();
+            var response = new List<User>();
+
+            foreach (var obj in users)
+            {
+                response.Add(new User()
+                {
+                    User_ID = obj.User_ID,
+                    Employee_Id = obj.Employee_Id,
+                    FirstName = obj.FirstName,
+                    LastName = obj.LastName,
+                    Project = obj.Project,
+                    Project_ID = obj.Project_ID,
+                    Task = obj.Task,
+                    Task_ID = obj.Task_ID
+                });
+            }
+            return response;
+        }
+        // GET: api/Users/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult GetUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/Users/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUser(int id, User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.User_ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Users
+        [ResponseType(typeof(User))]
+        public IHttpActionResult PostUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Users.Add(user);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = user.User_ID }, user);
+        }
+
+        //[HttpGet]
+        //// DELETE: api/Users/5
+        //[ResponseType(typeof(User))]
+        //public IHttpActionResult DeleteUser(int id)
+        //{
+        //    User user = db.Users.Find(id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    db.Users.Remove(user);
+        //    db.SaveChanges();
+
+        //    return Ok(user);
+        //}
+
+        [HttpDelete]
+        [ResponseType(typeof(User))]
+        public IHttpActionResult DeleteUserFromDB(int id)
+        {
+
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.Users.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserExists(int id)
+        {
+            return db.Users.Count(e => e.User_ID == id) > 0;
+        }
+    }
+}
